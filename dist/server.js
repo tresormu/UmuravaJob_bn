6c2bd0b7b176006f309162adfc26a2ae83d99e75
tresor <tresormugisha07@gmail.com>
2026-04-09 11:31:05 +0200

    Applicants CRUD

--- dist/server.js
+++ dist/server.js
@@ -6,6 +6,7 @@ import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import config from "./config/env.config.js";
+import ApplicantsRoutes from "./Routes/Applicant.route.js";
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -30,7 +31,9 @@ mongoose
     .connect(config.mongoUrl)
     .then(() => console.log(" Connected to MongoDB Compass"))
     .catch((err) => console.error(" Connection error:", err));
+app.use("/api/applicants", ApplicantsRoutes);
 app.listen(config.port, () => {
     console.log(`Server is running on http://localhost:${config.port}`);
+    console.log(`Swagger is running on http://localhost:${config.port}/api-docs`);
 });
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import cors from "cors";
 import helmet from "helmet";
 import rateLimit from "express-rate-limit";
 import config from "./config/env.config.js";
-import ApplicantsRoutes from "./Routes/Applicant.route.js"
+import ApplicantsRoutes from "./Routes/Applicant.route.js";
 const app = express();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
@@ -40,7 +40,8 @@ mongoose
   .then(() => console.log(" Connected to MongoDB Compass"))
   .catch((err) => console.error(" Connection error:", err));
 
-app.use("api/Applicants", ApplicantsRoutes)
+app.use("/applicants", ApplicantsRoutes);
 app.listen(config.port, () => {
   console.log(`Server is running on http://localhost:${config.port}`);
+  console.log(`Swagger is running on http://localhost:${config.port}/api-docs`)
 });
